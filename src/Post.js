53b8558243201673067bab476ee9f4f348b1b84f
Devin Põlluste <devinpolluste@gmail.com>
2020-10-06 09:33:13 +0300

    Header and Post component

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            <h3>Username</h3>
+
+            <img src="https://memegenerator.net/img/images/11708855.jpg" alt=""/>
+
+            <h4>Username: caption</h4>
+        </div>
+    )
+}
+
+export default Post
