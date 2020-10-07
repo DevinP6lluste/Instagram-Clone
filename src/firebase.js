61e5d723f8eaf3f6b7a5578dbd1b228c8f1dc0f8
Devin Põlluste <devinpolluste@gmail.com>
2020-10-07 13:37:02 +0300

    Authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 
-const firebaseApp = firebase.initalizeApp({
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBudh20-MO_8d6knva-TnUFpY82WcpRl-Y",
     authDomain: "instagram-clone-react-a11f4.firebaseapp.com",
     databaseURL: "https://instagram-clone-react-a11f4.firebaseio.com",
@@ -9,7 +9,7 @@ const firebaseApp = firebase.initalizeApp({
     messagingSenderId: "1012438850428",
     appId: "1:1012438850428:web:5377b5eb84524f6cf262f9",
     measurementId: "G-G07NE5806X"
-  });
+});
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
